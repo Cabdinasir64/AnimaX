f85a095c7a220e0861938ca924253d2c06c330e7
Cabdinasir64 <cabdinasira64@gmail.com>
2025-05-11 14:47:26 +0300

    i only make animation coming when ended navbar to start hero

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,14 +1,16 @@
-import Navbar from "../../Components/Navbar.jsx"
-import { motion } from "framer-motion";
+import { useState } from "react";
+import Navbar from "../../Components/Navbar.jsx";
+import Hero from './components/hero.jsx';
 
 const Home = () => {
+  const [heroAnimationStarted, setHeroAnimationStarted] = useState(false);
 
-    return (
-        <>
-            <Navbar />
-        </>
-    );
+  return (
+    <>
+      <Navbar onAnimationComplete={() => setHeroAnimationStarted(true)} />
+      <Hero startAnimation={heroAnimationStarted} />
+    </>
+  );
+};
 
-}
 export default Home;
-
